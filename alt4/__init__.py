110521d56674ad71953d92e50b364c5cb9bd8ecc
Billcountry <me@billcountry.tech>
2021-06-11 15:09:19 +0300

    Import logging methods

--- alt4/__init__.py
+++ alt4/__init__.py
@@ -1,2 +1,4 @@
 from alt4.settings import MODE_RELEASE, MODE_DEBUG, MODE_SILENT, MODE_TESTING
 from alt4.settings import set_source, set_auth_token, set_mode
+
+from alt4.logging import Logger, open_group, close_group, debug, info, fatal, warning, error
